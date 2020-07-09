29bf8dc1a419cb9f055267e208baf29ba13f2057
Igor Godoy <godoy.ivgm@gmail.com>
2020-07-09 12:31:14 -0300

    Continuing Dashboard page

--- mobile/src/hooks/auth.tsx
+++ mobile/src/hooks/auth.tsx
@@ -8,9 +8,16 @@ import React, {
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInData {
@@ -19,7 +26,7 @@ interface SignInData {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn: (data: SignInData) => Promise<void>;
   signOut: () => void;
@@ -39,6 +46,8 @@ export const AuthProvider: React.FC = ({ children }: any) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -60,6 +69,8 @@ export const AuthProvider: React.FC = ({ children }: any) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3336',
+  baseURL: 'http://192.168.15.7:3333',
 });
 
 export default api;
